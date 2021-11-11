1e2cc94d603cf99751340b6e1fe7d123e80c6b03
Jinghao Zhang <zhangjinghao1115@hotmail.com>
2021-11-11 15:50:02 -0800

    Midway Demo: added table, cue stick, and a ball model with default start texture for demo purpose only

--- pool.js
+++ pool.js
@@ -1,8 +1,10 @@
 import {defs, tiny} from './src/common.js';
 import {Body} from "./src/body.js";
 
+import {Shape_From_File} from './examples/obj-file-demo.js'
+
 // Pull these names into this module's scope for convenience:
-const {vec3, unsafe3, vec4, color, Mat4, Light, Shape, Material, Shader, Texture, Scene} = tiny;
+const {vec3, unsafe3, vec4, color, hex_color, Mat4, Light, Shape, Material, Shader, Texture, Scene} = tiny;
 
 export class Simulation extends Scene {
     // **Simulation** manages the stepping of simulation time.  Subclass it when making
@@ -85,6 +87,7 @@ export class Test_Data {
             grid: new Texture("assets/grid.png"),
             stars: new Texture("assets/stars.png"),
             text: new Texture("assets/text.png"),
+            //wood: new Texture("assets/wood.png"),
         }
         this.shapes = {
             donut: new defs.Torus(15, 15, [[0, 2], [0, 1]]),
@@ -95,6 +98,9 @@ export class Test_Data {
             prism: new (defs.Capped_Cylinder.prototype.make_flat_shaded_version())(10, 10, [[0, 2], [0, 1]]),
             gem: new (defs.Subdivision_Sphere.prototype.make_flat_shaded_version())(2),
             donut2: new (defs.Torus.prototype.make_flat_shaded_version())(20, 20, [[0, 2], [0, 1]]),
+            //background
+            "pooltable": new Shape_From_File("assets/background/pool_table.obj"),
+            "cuestick": new Shape_From_File("assets/background/cue_stick.obj"),
         };
     }
 
@@ -113,9 +119,11 @@ export class Pool_Scene extends Simulation {
         this.data = new Test_Data();
         this.shapes = Object.assign({}, this.data.shapes);
         this.shapes.square = new defs.Square();
+      
+
         const shader = new defs.Fake_Bump_Map(1);
         this.material = new Material(shader, {
-            color: color(.4, .8, .4, 1),
+            color: hex_color("#eeeee4"),
             ambient: .4, texture: this.data.textures.stars
         })
     }
@@ -146,7 +154,9 @@ export class Pool_Scene extends Simulation {
 
     display(context, program_state) {
         // display(): Draw everything else in the scene besides the moving bodies.
-        super.display(context, program_state);
+
+        //first, draw everything inherit from parent class, all the moving objects in this case
+        //super.display(context, program_state);
 
         if (!context.scratchpad.controls) {
             this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
@@ -156,15 +166,35 @@ export class Pool_Scene extends Simulation {
         program_state.projection_transform = Mat4.perspective(Math.PI / 4, context.width / context.height, 1, 500);
         program_state.lights = [new Light(vec4(0, -5, -10, 1), color(1, 1, 1, 1), 100000)];
         // Draw the ground:
-        this.shapes.square.draw(context, program_state, Mat4.translation(0, -10, 0)
-                .times(Mat4.rotation(Math.PI / 2, 1, 0, 0)).times(Mat4.scale(50, 50, 1)),
-            this.material.override(this.data.textures.earth));
+//         this.shapes.square.draw(context, program_state, Mat4.translation(0, -10, 0)
+//                 .times(Mat4.rotation(Math.PI / 2, 1, 0, 0)).times(Mat4.scale(50, 50, 1)),
+//             this.material.override(this.data.textures.earth));
+
+        //Draw the table:
+        let model_transform = Mat4.identity();
+        model_transform = model_transform
+                            .times(Mat4.rotation(1/3 *Math.PI, 1, 1, 1))
+                            .times(Mat4.translation(-3,-5,-10))
+                            .times(Mat4.scale(15, 15, 15));
+        
+        this.shapes.cuestick.draw(context, program_state, model_transform, this.material);
+        
+        model_transform = Mat4.identity();
+        model_transform = model_transform
+                            .times(Mat4.translation(0,-10,0))
+                            .times(Mat4.scale(25, 25, 25));
+        this.shapes.pooltable.draw(context, program_state, model_transform, this.material);
+        model_transform = Mat4.identity();
+        model_transform = model_transform
+                            .times(Mat4.translation(5,-5,10));
+                  
+        this.shapes.ball.draw(context, program_state, model_transform, this.material);
     }
 
-    show_explanation(document_element) {
-        document_element.innerHTML += `<p>This demo lets random initial momentums carry bodies until they fall and bounce.  It shows a good way to do incremental movements, which are crucial for making objects look like they're moving on their own instead of following a pre-determined path.  Animated objects look more real when they have inertia and obey physical laws, instead of being driven by simple sinusoids or periodic functions.
-                                     </p><p>For each moving object, we need to store a model matrix somewhere that is permanent (such as inside of our class) so we can keep consulting it every frame.  As an example, for a bowling simulation, the ball and each pin would go into an array (including 11 total matrices).  We give the model transform matrix a \"velocity\" and track it over time, which is split up into linear and angular components.  Here the angular velocity is expressed as an Euler angle-axis pair so that we can scale the angular speed how we want it.
-                                     </p><p>The forward Euler method is used to advance the linear and angular velocities of each shape one time-step.  The velocities are not subject to any forces here, but just a downward acceleration.  Velocities are also constrained to not take any objects under the ground plane.
-                                     </p><p>This scene extends class Simulation, which carefully manages stepping simulation time for any scenes that subclass it.  It totally decouples the whole simulation from the frame rate, following the suggestions in the blog post <a href=\"https://gafferongames.com/post/fix_your_timestep/\" target=\"blank\">\"Fix Your Timestep\"</a> by Glenn Fielder.  Buttons allow you to speed up and slow down time to show that the simulation's answers do not change.</p>`;
-    }
+//     show_explanation(document_element) {
+//         document_element.innerHTML += `<p>This demo lets random initial momentums carry bodies until they fall and bounce.  It shows a good way to do incremental movements, which are crucial for making objects look like they're moving on their own instead of following a pre-determined path.  Animated objects look more real when they have inertia and obey physical laws, instead of being driven by simple sinusoids or periodic functions.
+//                                      </p><p>For each moving object, we need to store a model matrix somewhere that is permanent (such as inside of our class) so we can keep consulting it every frame.  As an example, for a bowling simulation, the ball and each pin would go into an array (including 11 total matrices).  We give the model transform matrix a \"velocity\" and track it over time, which is split up into linear and angular components.  Here the angular velocity is expressed as an Euler angle-axis pair so that we can scale the angular speed how we want it.
+//                                      </p><p>The forward Euler method is used to advance the linear and angular velocities of each shape one time-step.  The velocities are not subject to any forces here, but just a downward acceleration.  Velocities are also constrained to not take any objects under the ground plane.
+//                                      </p><p>This scene extends class Simulation, which carefully manages stepping simulation time for any scenes that subclass it.  It totally decouples the whole simulation from the frame rate, following the suggestions in the blog post <a href=\"https://gafferongames.com/post/fix_your_timestep/\" target=\"blank\">\"Fix Your Timestep\"</a> by Glenn Fielder.  Buttons allow you to speed up and slow down time to show that the simulation's answers do not change.</p>`;
+//     }
 }
